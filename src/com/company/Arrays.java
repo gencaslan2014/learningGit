110670e4feed12dd4c2832d3aa338212645ad262
gencaslan2014@gmail.com <gencaslan2014@gmail.com>
2022-07-22 13:51:28 +0100

    added additionOfElement method

--- src/com/company/Arrays.java
+++ src/com/company/Arrays.java
@@ -1,8 +1,76 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Arrays {
-    public void printHello(){
-        System.out.println("Hello World");
-        System.out.println("Hello World Again");
+    Scanner sc = new Scanner(System.in);
+
+    public void deletionOfElement(int[] array){
+        System.out.println("Here is the current array:");
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        System.out.println("Please tell us which number you want to delete:");
+        int userChoice = sc.nextInt();
+        int counter = 0;
+
+        for (int i=0;i<array.length;i++){
+            if (userChoice!=array[i]){
+                array[counter]=array[i];
+                counter++;
+            }
+        }
+
+        for (int i=0;i<counter;i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+        int deletion = array.length-counter;
+        System.out.println("The array was having "+array.length+" elements");
+        System.out.println("The number got deleted "+deletion+" times");
+
+        int[] newArray = new int[counter];
+
+        for (int i=0;i<counter;i++){
+            newArray[i] = array[i];
+        }
+
+        System.out.println("Here is your newArray:");
+        for (int i=0;i<newArray.length;i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+
+    public void additionOfElement(int[] array){
+        System.out.println("Here is the current array:");
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        System.out.println("Please tell us which number you want to add:");
+        int userNumber = sc.nextInt();
+        System.out.println("Please tell us at which Index you want to add this number:");
+        int numberIndex = sc.nextInt();
+        int newLength = array.length+1;
+
+        int [] newArray = new int[newLength];
+        System.out.println("The previous array was having the length "+array.length);
+        System.out.println("A newArray with the length "+ newArray.length +" has been created");
+
+        for (int i=0;i<numberIndex;i++){
+            newArray[i] = array[i];
+        }
+
+        newArray[numberIndex] = userNumber;
+
+        for (int i=numberIndex;i<array.length;i++){
+            newArray[i+1]=array[i];
+        }
+
+        System.out.println("Here is your newArray:");
+        for (int j : newArray) {
+            System.out.print(j + " ");
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Arrays arrays = new Arrays();
+
+        int[] array = {10,15,99,99,99,99,99,88,99,99,99,99,99,10,15,99,99,99,99,99,77,99,99,99,99,99,10,15,99,99,99,99,99,99,99,99,99,99};
+
+        arrays.additionOfElement(array);
     }
 }
